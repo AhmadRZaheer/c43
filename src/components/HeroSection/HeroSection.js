a10d378b41c1c55b70b78f2846062189e6699172
Talha Irshad <talhairshad97@gmail.com>
2022-10-12 21:48:30 +0500

    Animations done

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Wrapper from '../Helpers/Wrapper'
 import classes from './HeroSection.module.css';
-import Object from '../../assets/object.png'
+import Object from '../../assets/object.png';
+import C from '../../assets/c.png';
 import { motion } from 'framer-motion';
 
 const HeroSection = (props) => {
@@ -10,21 +11,31 @@ const HeroSection = (props) => {
             <div className={classes.bg_overlay}>
                 <motion.img 
                     transition={{ type: 'spring', stiffness: 10,delay: 1}} 
-                    animate={{x: -200, y: -50, scale: 0.8}} 
+                    animate={{x: -400, y: -120, scale: 0.3}} 
+                    className={classes.imgobj} 
+                    src={Object} alt="Object" />
+                
+                <motion.img
+                    initial={{opacity: 0}} 
+                    transition={{ type: 'spring', stiffness: 10,delay: 1}} 
+                    animate={{x: 300, y: -150, scale: 0.2, opacity: 1}} 
+                    className={classes.imgobj} 
+                    src={Object} alt="Object" />
+                
+                <motion.img
+                    initial={{opacity: 0}}
+                    transition={{ type: 'spring', stiffness: 10,delay: 1}} 
+                    animate={{x: 400, y: 200, scale: 0.3, opacity: 1}} 
                     className={classes.imgobj} 
                     src={Object} alt="Object" />
                 
                 <motion.img 
-                    transition={{ type: 'spring', stiffness: 10,delay: 1}} 
-                    animate={{x: -200, y: -50, scale: 0.8}} 
-                    className={classes.imgobj} 
-                    src={Object} alt="Object" />
-                
-                <motion.img 
-                    transition={{ type: 'spring', stiffness: 10,delay: 1}} 
-                    animate={{x: -200, y: -50, scale: 0.8}} 
-                    className={classes.imgobj} 
-                    src={Object} alt="Object" />
+                    src={C} 
+                    alt="C" 
+                    className={classes.c}
+                    initial={{opacity: 0}}
+                    transition={{delay:2, default: { ease: "easeInOut" }}}
+                    animate={{opacity:1}}  />
                 
             </div>
             <div className={classes.container}>
