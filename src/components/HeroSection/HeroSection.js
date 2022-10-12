82fe0b5c79a08bbe8aa00a10ca44317d8cd27e2a
Talha Irshad <talhairshad97@gmail.com>
2022-10-12 18:44:53 +0500

    Created Hero Seection, framer motion installed

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from '../Helpers/Wrapper'
+
+const HeroSection = (props) => {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  )
+}
+
+export default HeroSection;
