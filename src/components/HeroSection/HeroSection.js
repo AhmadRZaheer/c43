8af4d5b2260ca3b77fba3c01a2b919ed49101417
Talha Irshad <talhairshad97@gmail.com>
2022-10-13 19:32:28 +0500

    Code refactored

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -3,7 +3,7 @@ import Wrapper from '../Helpers/Wrapper'
 import classes from './HeroSection.module.css';
 import Animation from './Animation';
 import Arrow from '../../assets/arrow.png';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const HeroSection = (props) => {
     return (
@@ -13,16 +13,16 @@ const HeroSection = (props) => {
                 <div className={classes.hero_text}>
                     <p className={classes.head}>Not Just</p>
                     <h1 className={classes.head_mid}>BUZZ-WORDS</h1>
-                    <p className={classes.head_btm}>Create 
-                    Marketing Solutions for Disruptors.</p>
+                    <p className={classes.head_btm}>Create
+                        Marketing Solutions for Disruptors.</p>
                 </div>
             </div>
-            <motion.img 
-                className={classes.arrow} 
+            <motion.img
+                className={classes.arrow}
                 src={Arrow} alt="Arrow"
-                transition={{delay: 3, type: 'spring'}} 
-                initial={{y: -60, opacity: 0}} 
-                animate={{y: 0, opacity: 1}} />
+                transition={{ delay: 3, type: 'spring' }}
+                initial={{ y: -60, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }} />
         </Wrapper>
     )
 }
