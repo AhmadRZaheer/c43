7c9ebb3304bda05fce994283eff441d9c68c1a9f
Talha Irshad <talhairshad97@gmail.com>
2022-10-18 20:35:05 +0500

    Added sections

---
+++ src/components/Sections/Section.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Wrapper from '../Helpers/Wrapper'
+import SectionOne from './SectionOne'
+
+const Section = (props) => {
+  return (
+    <Wrapper>
+        <SectionOne />
+    </Wrapper>
+  )
+}
+
+export default Section
---
+++ src/components/Sections/SectionOne.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Wrapper from '../Helpers/Wrapper';
+import classes from './SectionOne.module.css'
+
+const SectionOne = (props) => {
+  return (
+    <Wrapper>
+        
+    </Wrapper>
+  )
+}
+
+export default SectionOne
