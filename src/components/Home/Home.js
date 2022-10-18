e3039b242b814738d03ca415d56f572c5bb4cb60
Talha Irshad <talhairshad97@gmail.com>
2022-10-18 20:36:27 +0500

    Added Section component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Wrapper from '../Helpers/Wrapper'
 import Navbar from '../UI/Navbar'
 import HeroSection from '../HeroSection/HeroSection'
+import Section from '../Sections/Section'
 
 const Home = (props) => {
   return (
     <Wrapper>
         <Navbar />
         <HeroSection />
+        <Section />
     </Wrapper>
   )
 }
