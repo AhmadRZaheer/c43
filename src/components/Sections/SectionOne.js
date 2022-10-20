3a7395b186bc77519ef87361d09da5371661916e
Talha Irshad <talhairshad97@gmail.com>
2022-10-20 23:12:11 +0500

    Animation added in SectionOne.js

--- src/components/Sections/SectionOne.js
+++ src/components/Sections/SectionOne.js
@@ -2,27 +2,69 @@ import React, { useEffect } from 'react';
 import Wrapper from '../Helpers/Wrapper';
 import classes from './SectionOne.module.css'
 import Object from '../../assets/object.png'
-import { motion, useAnimation } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 const SectionOne = (props) => {
 
-  const {ref, inView} = useInView();
-  const animation = useAnimation()
+  const {ref, inView} = useInView({
+    threshold: 0.4
+  });
+  const textanimation = useAnimation()
+  const imganimation = useAnimation()
+
 
   useEffect(() => {
-    console.log('Use effect hook, inView=', inView)
+    if(!inView) {
+      textanimation.start({
+        x:0, opacity: 0 
+      })
+    }
+    if (inView) {
+      textanimation.start({
+        x: -300,
+        opacity:1,
+        transition:{
+          type: 'spring',
+          bounce: 0.3,
+          delay: 1.25
+        }
+      })
+    }
+    if(!inView) {
+      imganimation.start({
+        x:0,
+        transition:{
+          type: 'spring',
+          stiffness:50
+        }
+      })
+    }
+    if(inView){
+      imganimation.start({
+        x:1000,
+        transition:{
+          type: 'tween',
+          delay: 1
+        }
+      })
+    }
   }, [inView]);
 
   return (
     <Wrapper>
       <div className={classes.container}>
         <div ref={ref} className={classes.box}>
-          <motion.h1 animate={{x: -200}} className={classes.text}>
+          <motion.h1  
+            animate={textanimation} 
+            className={classes.text}>
             Our Partners Choose us for their Growth Journey
             because we are willing to go hunting with them.</motion.h1>
 
-          <motion.img animate={{x: 1000}} src={Object} alt="Object" className={classes.img} />
+          <motion.img 
+            animate={imganimation} 
+            src={Object} alt="Object" 
+            className={classes.img} />
 
         </div>
       </div>
