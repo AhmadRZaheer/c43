c66c846f12087ae9152af0f61511cb21e74e6f79
Talha Irshad <talhairshad97@gmail.com>
2022-10-21 21:00:16 +0500

    Added new component SectionTwo.js

--- src/components/Sections/Section.js
+++ src/components/Sections/Section.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Wrapper from '../Helpers/Wrapper'
 import SectionOne from './SectionOne'
+import SectionTwo from './SectionTwo'
 
 const Section = (props) => {
   return (
     <Wrapper>
         <SectionOne />
+        <SectionTwo />
     </Wrapper>
   )
 }
---
+++ src/components/Sections/SectionTwo.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import classes from './SectionTwo.module.css';
+import Wrapper from '../Helpers/Wrapper';
+import {motion, useAnimation} from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const SectionTwo = (props) => {
+  return (
+    <Wrapper>
+      <div className={classes.container}>
+        <motion.h1 className={classes.heading}>Trust is hard earned commodity, and
+            we earns yours through results.
+        </motion.h1>
+        <motion.button className={classes.button} type="button">Coffee?</motion.button>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default SectionTwo
