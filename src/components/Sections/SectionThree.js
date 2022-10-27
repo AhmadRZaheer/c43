17e64e6a2f26867e3671608d1f5a38caff389df6
Talha Irshad <talhairshad97@gmail.com>
2022-10-27 17:54:58 +0500

    Added Animation in Section3

--- src/components/Sections/SectionThree.js
+++ src/components/Sections/SectionThree.js
@@ -5,13 +5,35 @@ import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 const SectionThree = (props) => {
+
+  const {ref, inView} = useInView({
+    threshold: 0
+  })
+
+  const outerCircle = useAnimation();
+  const innerCircle = useAnimation();
+
+  useEffect(() => {
+    if (!inView) {
+      outerCircle.start({
+        opacity: 0,
+        transition: {
+          type: 'spring',
+          bounce: 0
+        }
+      })
+    }
+  })
+
   return (
     <Wrapper>
-        <div className={classes.outercircle}>
-            <div className={classes.innercircle}>
+      <div ref={ref}>
+        <motion.div className={classes.outercircle}>
+            <motion.div className={classes.innercircle}>
                 
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
+      </div>
     </Wrapper>
   )
 }
